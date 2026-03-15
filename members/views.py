139a61e917bb48eb58ab14c07c317a09def3e603
atobosomtwi <atobosomtwi90@gmail.com>
2026-03-15 06:49:35 +0000

    Modified Views

--- members/views.py
+++ members/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Members
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome home")
+    # members = Members.objects.get(id = pk)
+    members = Members.objects.all()
+
+    output = ""
+    for member in members:
+        output += f"{member.full_name} - {member.email}<br>"
+
+    return HttpResponse(output)
+
