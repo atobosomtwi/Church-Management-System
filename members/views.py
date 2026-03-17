7f38fcdf5e3b308566d87335ba8d4670b1259877
atobosomtwi <atobosomtwi90@gmail.com>
2026-03-17 13:23:45 +0000

    Added Template Dir

--- members/views.py
+++ members/views.py
@@ -4,12 +4,7 @@ from . models import Members
 
 # Create your views here.
 def home(request):
-    # members = Members.objects.get(id = pk)
     members = Members.objects.all()
-
-    output = ""
-    for member in members:
-        output += f"{member.full_name} - {member.email}<br>"
-
-    return HttpResponse(output)
+    
+    return render(request, 'home.html', {'members': members})
 
